3e5fed2b15b5d4887e6d22bfbfeee5344ba676ed
Kevin Pita <gitkevin@pm.me>
2026-08-27 12:07:49 +0200

    fix: make dms.output optional during startup

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -1,4 +1,4 @@
-require("dms.outputs")
+pcall(require, "dms.outputs")
 
 hl.config({
     general = {
@@ -296,4 +296,4 @@ hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
-require("dms.layout")
+pcall(require, "dms.layout")
