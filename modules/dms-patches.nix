6c1345f7c0d583e2b8a8c3f0b93e664a3d6ed6e9
Kevin Pita <gitkevin@pm.me>
2026-08-28 14:12:51 +0200

    feat: drag to re-order workspaces

--- modules/dms-patches.nix
+++ modules/dms-patches.nix
@@ -3,6 +3,10 @@
     (_: prev: {
       dms-shell = prev.dms-shell.overrideAttrs (old: {
         postInstall = (old.postInstall or "") + ''
+          workspace_switcher=$out/share/quickshell/dms/Modules/DankBar/Widgets/WorkspaceSwitcher.qml
+          chmod u+w "$(dirname "$workspace_switcher")" "$workspace_switcher"
+          patch -d $out/share/quickshell/dms -p2 < ${../patches/workspace-switcher-drag-reorder.patch}
+
           substituteInPlace $out/share/quickshell/dms/Modules/ControlCenter/BuiltinPlugins/TailscaleWidget.qml \
             --replace-fail 'color: peerMouseArea.containsMouse ? Theme.primaryHoverLight : Theme.surfaceLight' \
             'color: peerMouseArea.containsMouse ? Theme.primaryContainer : Theme.surfaceContainerHighest' \
